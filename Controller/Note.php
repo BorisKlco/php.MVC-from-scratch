0ba29282f82d3887f3a08f3a248c7abb5d2c7196
BorisKlco <klco@pm.me>
2024-11-20 22:21:39 +0100

    Creating a note, Showing list of notes

--- Controller/Note.php
+++ Controller/Note.php
@@ -4,12 +4,15 @@ namespace Controller;
 
 use Core\Database;
 use Core\View;
+use Helper\Validator;
 
 class Note
 {
     public function index()
     {
-        View::show('note/index', ['title' => 'Notes', 'something' => 'testSomething']);
+        $email = $_SESSION['user'];
+        $notes = Database::query('SELECT title, created_at, link FROM notes WHERE email = ?', [$email])->fetchAll() ?? [];
+        View::show('note/index', ['title' => 'Notes', 'notes' => $notes]);
     }
 
     public function show()
@@ -24,7 +27,35 @@ class Note
 
     public function store()
     {
-        dd(request());
+        verifyCsrf(request()->get('_csrf'));
+        $title = request()->get('title');
+        $note = request()->get('note');
+
+        if (!Validator::string($note, 1, 5000) || !Validator::string($title, 1, 72)) {
+            $error['note'] = "Note no more than 5000 characters, Title no more than 72 characters.";
+            View::show('note/create', [
+                'title' => 'Create',
+                'noteTitle' => $title,
+                'note' => $note,
+                'error' => $error
+            ]);
+        }
+
+        do {
+            $link = uniqid();
+            $isUnique = Database::query('SELECT link FROM notes WHERE link = ?', [$link])->fetch();
+        } while ($isUnique);
+
+        $store = Database::query("
+        INSERT INTO notes (link, title,note,email)
+        VALUES (:link, :title, :note, :email);
+        ", [
+            'link' => $link,
+            'title' => $title,
+            'note' => $note,
+            'email' => $_SESSION['user'],
+        ]);
+        redirect('/notes');
     }
 
     public function edit()
--- views/note/create.php
+++ views/note/create.php
@@ -22,14 +22,12 @@ function placeholder()
     <div class="overflow-hidden rounded-lg border border-gray-300 shadow-sm 
     focus-within:border-sky-500 focus-within:ring-1 focus-within:ring-sky-500">
         <input type="text" name="title" id="title" class="block w-full border-0 pt-2.5 text-lg font-medium 
-        placeholder:text-gray-400 focus:ring-0" placeholder="Title">
-        <textarea rows="2" name="description" id="description" class="block w-full resize-none border-0 py-0 text-gray-900 
-        placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="<?= placeholder() ?>"></textarea>
+        placeholder:text-gray-400 focus:ring-0" placeholder="Title" value="<?= $noteTitle ?? '' ?>">
+        <textarea rows="2" name="note" id="note" class="block w-full min-h-36 border-0 py-0 text-gray-900 
+        placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="<?= placeholder() ?>"><?= $note ?? '' ?></textarea>
         <!-- Spacer element to match the height of the toolbar -->
         <div aria-hidden="true">
-            <div class="py-2">
-                <div class="h-9"></div>
-            </div>
+
             <div class="h-px"></div>
             <div class="py-2">
                 <div class="py-px">
@@ -41,19 +39,13 @@ function placeholder()
 
     <div class="absolute inset-x-px bottom-0">
         <div class="flex items-center justify-between space-x-3 border-t border-gray-200 px-2 py-2 sm:px-3">
-            <div class="flex">
-                <button type="button" class="group -my-2 -ml-2 inline-flex items-center rounded-full px-3 py-2 text-left text-gray-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
-                    </svg>
-                </button>
-            </div>
-            <div class="flex-shrink-0">
-                <button type="submit" class="inline-flex items-center rounded-md bg-sky-400 
+            <?php if ($error['note'] ?? false) : ?>
+                <span class="text-sm text-red-600 px-2"><?= $error['note'] ?></span>
+            <?php endif ?>
+            <button type="submit" class="inline-flex ml-auto items-center rounded-md bg-sky-400 
                 px-3 py-2 text-sm font-semibold text-white shadow-sm 
                 hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 
                 focus-visible:outline-offset-2 focus-visible:outline-sky-600">Create</button>
-            </div>
         </div>
     </div>
 </form>
